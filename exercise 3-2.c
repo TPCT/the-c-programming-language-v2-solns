9bb7b7aaa6b983e9665c75fc0b8fda2c510a318c
TPCT <eslamjackie@outlook.com>
2020-11-25 01:56:25 +0200

    Update exercise 3-2.c

--- exercise 3-2.c
+++ exercise 3-2.c
@@ -3,17 +3,49 @@
 #define STRING_SIZE 100
 
 void escape(char s[], char t[]);
+void unescape(char s[], char t[]);
 
 int main(void){
  	char s[STRING_SIZE] = "hello world i love python";
  	char t[STRING_SIZE] = "do	while\n";
+ 	char x[STRING_SIZE];
  	escape(s, t);
+ 	unescape(x, s);
  	printf("%s\n", s);
+ 	printf("%s\n", x);
  	return 0;
+}
+
+void unescape(char s[], char t[]){
+	int i, j;
+
+	for(i=0; s[i] != '\0'; i++);
+	for(j=0; t[j] != '\0'; j++, i++){
+		switch(t[j]){
+			case '\\':
+				if (t[j+1] == 'n'){
+					s[i] = '\n';
+					j++;
+				}
+				else if (t[j+1] == 't'){
+					s[i] = '\t';
+					j++;
+				}
+				else
+					s[i] = '\\';
+				break;
+			default:
+				s[i] = t[j];
+				break;
+		}
+	}
+
+	s[i] = '\0';
 }
 
 void escape(char s[], char t[]){
 	int i, j;
+
 	for (i=0; s[i] != '\0'; i++);
 	for (j=0; t[j] != '\0'; j++, i++){
 		switch (t[j]){
@@ -30,5 +62,6 @@ void escape(char s[], char t[]){
 				break; 
 		}
 	}
+
 	s[i] = '\0';
 }
