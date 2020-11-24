1fe46629fe5623fb182a49b1aec16f9d759396c1
TPCT <eslamjackie@outlook.com>
2020-11-24 21:30:30 +0200

    Create exercise 3-2.c

---
+++ exercise 3-2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define STRING_SIZE 100
+
+void escape(char s[], char t[]);
+
+int main(void){
+ 	char s[STRING_SIZE] = "hello world i love python";
+ 	char t[STRING_SIZE] = "do	while\n";
+ 	escape(s, t);
+ 	printf("%s\n", s);
+ 	return 0;
+}
+
+void escape(char s[], char t[]){
+	int i, j;
+	for (i=0; s[i] != '\0'; i++);
+	printf("%d\n", i);
+	for (j=0; t[j] != '\0'; j++, i++){
+		switch (t[j]){
+			case '\n':
+				s[i++] = '\\';
+				s[i]   = 'n';
+				break; 
+			case '\t':
+				s[i++] = '\\';
+				s[i]   = 't';
+				break;
+			default:
+				s[i] = t[j];
+				break; 
+		}
+	}
+	s[i] = '\0';
+}
